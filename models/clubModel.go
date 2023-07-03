dee827479ed9e88b2db472a13e932b0df6eb3890
Juan Navarro Sr <juan.navarro68@gmail.com>
2023-07-03 12:42:07 -0600

    After merge

--- models/clubModel.go
+++ models/clubModel.go
@@ -30,24 +30,3 @@ type Court struct {
 	Indoors   bool
 	ClubID    uuid.UUID
 }
-package models
-
-import (
-	"time"
-
-	"github.com/google/uuid"
-	"gorm.io/gorm"
-)
-
-type Club struct {
-	ID          uuid.UUID `gorm:"primary_key;type:uuid;default:gen_random_uuid()"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
-	Name        string         `gorm:"unique"`
-	Description string
-	Contact     string
-	ImageURL    string
-	Address     string
-	Phone       string
-}
