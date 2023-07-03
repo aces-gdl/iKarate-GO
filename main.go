c942c734490d393d661d7741deca8945b31684c0
Juan Navarro Sr <juan.navarro68@gmail.com>
2023-07-03 12:32:37 -0600

    Users, canchas y clubs basicos

--- main.go
+++ main.go
@@ -21,5 +21,12 @@ func main() {
 	r.POST("/v1/security/login", controllers.Login)
 	r.GET("/v1/security/validate", middleware.RequireAuth, controllers.Validate)
 
+	r.POST("/v1/catalogs/club", middleware.RequireAuth, controllers.PostClub)
+	r.GET("/v1/catalogs/club", middleware.RequireAuth, controllers.GetClubs)
+
+	r.POST("/v1/catalogs/court", middleware.RequireAuth, controllers.PostCourts)
+	r.GET("/v1/catalogs/court", middleware.RequireAuth, controllers.GetCourts)
+	r.GET("/v1/catalogs/court/byclub", middleware.RequireAuth, controllers.GetCourtsByClub)
+
 	r.Run()
 }
---
+++ models/clubModel.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Club struct {
+	ID          uuid.UUID `gorm:"primary_key;type:uuid;default:gen_random_uuid()"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	Name        string         `gorm:"unique"`
+	Description string
+	Contact     string
+	ImageURL    string
+	Address     string
+	Phone       string
+	Courts      []Court `gorm:"foreignKey:ClubID"`
+}
+
+type Court struct {
+	ID        uuid.UUID `gorm:"primary_key;type:uuid;default:gen_random_uuid()"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Name      string
+	Indoors   bool
+	ClubID    uuid.UUID
+}
